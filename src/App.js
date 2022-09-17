bd6f2c7596bf26e8c8c987706e1f25c862d1ad1b
Jayant Verma <iamvermajayant@gmail.com>
2022-09-17 21:26:23 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import {Routes} from './components/Routes';
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className=" bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <Routes/>
+        <Footer/>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from  'react-router-dom';
 
-export const Navbar = () => {
+export const Navbar = ({setDarkTheme, darkTheme}) => {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className="p-5 pb-0 flex flex-warp sm:justify-between justify-center items-center border-b dark:border-gray-200 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+       <Link to="">
+          <p className="text-2xl font-bold py-1 px-2 text-white bg-blue-500 rounded dark:bg-gray-500 dark:text-gray-900">
+            Google 🔍
+          </p>
+       </Link>
+       <button type="button" onClick={() => setDarkTheme(!darkTheme)}>click</button>
+      </div>
     </div>
   )
 }
